a13e3a0cf9d5c17686d4e6151ab7fa644d6f2109
AlexanderBankov <81157794+AlexanderBankov@users.noreply.github.com>
2021-03-22 13:48:19 +0300

    Update bnso_scd2.sql

--- bnso_scd2.sql
+++ bnso_scd2.sql
@@ -62,20 +62,17 @@ WHEN matched THEN UPDATE SET
 
 
 -- 3.2.2 OBJECT
-INSERT INTO DEMIPT.BNSO_DWH_DIM_SCHEDULE (schedule_id, object_id, parent_id, created, modified, deleted, job_id, volume_actual, expenditure_actual, volume_sum, expenditure_sum, element_id, deleted_flg, effective_from, effective_to )
+INSERT INTO DEMIPT.BNSO_DWH_DIM_OBJECT_HIST ( object_id , project_id , created , modified , deleted , region_id , object_type_id , date_start , date_finish , deleted_flg, effective_from, effective_to )
 SELECT
-	schedule_id,
-	object_id,
-	parent_id,
-	created,
-	modified,
-	deleted,
-	job_id,
-	volume_actual,
-	expenditure_actual,
-	volume_sum,
-	expenditure_sum,
-	element_id,
+	object_id ,
+	project_id ,
+	created ,
+	modified ,
+	deleted ,
+	region_id ,
+	object_type_id ,
+	date_start ,
+	date_finish ,
 	CASE 
 		WHEN deleted IS NOT NULL
 		THEN 'Y'
@@ -83,20 +80,36 @@ SELECT
 	END AS deleted_flg,
 	COALESCE( modified, created ),
 	TO_DATE( '2999-12-31', 'YYYY-MM-DD' )
-FROM DEMIPT.BNSO_STG_SCHEDULE
-WHERE schedule_id NOT IN (
-	SELECT schedule_id FROM (
-        SELECT schedule_id, object_id, parent_id, created, modified, deleted, job_id, volume_actual, expenditure_actual, volume_sum, expenditure_sum, element_id
-        FROM DEMIPT.BNSO_STG_SCHEDULE
+FROM DEMIPT.BNSO_STG_OBJECTS
+WHERE object_id NOT IN (
+	SELECT object_id FROM (
+        SELECT object_id , project_id , created , modified , deleted , region_id , object_type_id , date_start , date_finish
+        FROM DEMIPT.BNSO_STG_OBJECTS
         INTERSECT
-        SELECT schedule_id, object_id, parent_id, created, modified, deleted, job_id, volume_actual, expenditure_actual, volume_sum, expenditure_sum, element_id
-        FROM DEMIPT.BNSO_DWH_DIM_SCHEDULE 
+        SELECT object_id , project_id , created , modified , deleted , region_id , object_type_id , date_start , date_finish
+        FROM DEMIPT.BNSO_DWH_DIM_OBJECT_HIST 
         )
 );
 
-MERGE INTO DEMIPT.BNSO_DWH_DIM_SCHEDULE tgt
-USING DEMIPT.BNSO_STG_SCHEDULE src
-ON ( tgt.schedule_id = src.schedule_id AND tgt.effective_from < coalesce( src.modified, src.created ) )
+MERGE INTO DEMIPT.BNSO_DWH_DIM_OBJECT_HIST tgt
+USING DEMIPT.BNSO_STG_OBJECTS src
+ON ( tgt.object_id = src.object_id AND tgt.effective_from < coalesce( src.modified, src.created ) )
 WHEN matched THEN UPDATE SET 
     tgt.effective_to = COALESCE( src.modified, src.created ) - interval '1' second
 		WHERE tgt.effective_to = to_date( '2999-12-31', 'YYYY-MM-DD' );
+
+
+-- 4. Обновляем метаданные - дату максимальной загрузуки
+UPDATE DEMIPT.BNSO_META_LOADING 
+SET last_update = ( SELECT MAX( created ) FROM DEMIPT.BNSO_STG_SCHEDULE_FACT )
+WHERE  dbname = 'DEMIPT' AND tablename = 'BNSO_DWH_FACT_SCHEDULE' AND ( SELECT max( created ) FROM DEMIPT.BNSO_STG_SCHEDULE_FACT ) IS NOT NULL;
+
+UPDATE DEMIPT.BNSO_META_LOADING 
+SET last_update = ( SELECT MAX( COALESCE( modified, created ) ) FROM DEMIPT.BNSO_STG_SCHEDULE )
+WHERE  dbname = 'DEMIPT' AND tablename = 'BNSO_DWH_DIM_SCHEDULE_HIST' AND ( SELECT max( COALESCE( modified, created ) ) FROM DEMIPT.BNSO_STG_SCHEDULE ) IS NOT NULL;
+
+UPDATE DEMIPT.BNSO_META_LOADING 
+SET last_update = ( SELECT max( COALESCE( modified, created ) ) FROM DEMIPT.BNSO_STG_OBJECTS )
+WHERE  dbname = 'DEMIPT' AND tablename = 'BNSO_DWH_DIM_OBJECT_HIST' AND ( SELECT MAX( COALESCE( modified, created ) ) FROM DEMIPT.BNSO_STG_OBJECTS ) IS NOT NULL;
+
+commit
